7f65c5162cabe808c5b330877da98f708fd76bd5
Ian_Lin <ian920511@gmail.com>
2023-07-26 23:47:12 +0800

    feat: add deploy

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,7 @@
 import axios  from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'http://localhost:3000/api'
+const baseURL = "https://shopping-2023-bb99fdfd4043.herokuapp.com/api"
 
 const axiosInstance = axios.create({
   baseURL
